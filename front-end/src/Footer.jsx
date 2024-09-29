d976bfa7f8a682621038450c41b9665698eec4d0
Chaitanya Mittal <132287085+ChaitanyaMittal27@users.noreply.github.com>
2024-09-28 17:54:46 -0700

    Create Footer.jsx
    footer

---
+++ front-end/src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+  return (
+    <>
+    <footer className="footer">
+      <p>&copy;2024 Faster than Light. All Rights Reserved.</p>
+    </footer>
+    </>
+  );
+}
+
+export default Footer;
